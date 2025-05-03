b183e467445e03c8c414865db2b680d6c2dae526
houssein ebe <hanimation410@gmail.com>
2025-05-03 10:26:24 +0000

    add image

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('image')->nullable();
             $table->string('registration_number')->unique();
             $table->date('hire_date')->nullable();
             $table->string('phone')->nullable();
